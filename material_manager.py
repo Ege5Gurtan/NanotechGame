f01dad02c8c7a36ad04d9bfe3de869b0e3260524
Ege5Gurtan <64253009+Ege5Gurtan@users.noreply.github.com>
2023-08-27 11:10:55 +0200

    Update material_manager.py

--- material_manager.py
+++ material_manager.py
@@ -1,25 +1,27 @@
 import bpy
 
-material_names = list(bpy.data.materials.keys())
-for material_name in material_names:
-    bpy.data.materials.remove(bpy.data.materials[material_name], do_unlink=True)
-    print(f"Material '{material_name}' deleted.")
+
 
 # Create a new material
-material_name = "ColoredMaterial32"
-new_material = bpy.data.materials.new(name=material_name)
-new_material.use_nodes = True
-
-principled_bsdf_node = new_material.node_tree.nodes["Principled BSDF"]
-
-principled_bsdf_node.inputs["Base Color"].default_value = (0,1,0,1)
-
-
-
 obj = bpy.data.objects["Cube"]
-obj.data.materials.clear()
-#material_index = obj.data.materials.find(material_name)
-#print(material_index)
-#obj.data.materials.pop(0)
-obj.data.materials.append(new_material)
 
+
+def delete_all_materials():
+    material_names = list(bpy.data.materials.keys())
+    for material_name in material_names:
+        bpy.data.materials.remove(bpy.data.materials[material_name], do_unlink=True)    
+
+def create_material(material_name,R,G,B,A):
+    new_material = bpy.data.materials.new(name=material_name)
+    new_material.use_nodes = True
+    principled_bsdf_node = new_material.node_tree.nodes["Principled BSDF"]
+    principled_bsdf_node.inputs["Base Color"].default_value = (R,G,B,A)
+    return new_material
+
+def assign_material(obj,material_to_be_assigned):
+    obj.data.materials.clear()
+    obj.data.materials.append(material_to_be_assigned)
+
+delete_all_materials()
+new_mat = create_material('sp',1,0,0,0)
+assign_material(obj,new_mat)
