79ad750c2ae8e0613ac5cd513de9303f01910549
Ege5Gurtan <64253009+Ege5Gurtan@users.noreply.github.com>
2023-09-23 18:30:17 +0200

    Update material_manager.py

--- material_manager.py
+++ material_manager.py
@@ -1,8 +1,6 @@
 import bpy
-<<<<<<< HEAD
 import os
 import importlib.util
-
 
 def include_module(module_path):
     spec = importlib.util.spec_from_file_location("my_module", module_path)
@@ -10,25 +8,10 @@ def include_module(module_path):
     spec.loader.exec_module(module)
     return module
 
-def delete_all_materials():
-    material_names = list(bpy.data.materials.keys())
-    for material_name in material_names:
-        bpy.data.materials.remove(bpy.data.materials[material_name], do_unlink=True)    
+def set_material_color(material,R,G,B,A):
+    principled_bsdf_node = material.node_tree.nodes["Principled BSDF"]
+    principled_bsdf_node.inputs["Base Color"].default_value = (R,G,B,A)
 
-=======
-
-
-
-# Create a new material
-obj = bpy.data.objects["Cube"]
-
-
-def delete_all_materials():
-    material_names = list(bpy.data.materials.keys())
-    for material_name in material_names:
-        bpy.data.materials.remove(bpy.data.materials[material_name], do_unlink=True)    
-
->>>>>>> f01dad02c8c7a36ad04d9bfe3de869b0e3260524
 def create_material(material_name,R,G,B,A):
     new_material = bpy.data.materials.new(name=material_name)
     new_material.use_nodes = True
@@ -36,20 +19,44 @@ def create_material(material_name,R,G,B,A):
     principled_bsdf_node.inputs["Base Color"].default_value = (R,G,B,A)
     return new_material
 
-def assign_material(obj,material_to_be_assigned):
+def assign_material(obj,material_to_be_assigned,grid,add_to_history=True):
     obj.data.materials.clear()
     obj.data.materials.append(material_to_be_assigned)
+    if add_to_history:
+        grid.cube_history[obj].append(material_to_be_assigned)
 
-<<<<<<< HEAD
+def check_if_cube_has_material(cube):
+    if hasattr(cube,"data") and hasattr(cube.data,"materials") and (len(cube.data.materials) == 1):
+        return True
+    else:
+        return False
 
+def check_if_cube_has_given_material(obj,material_to_check):
+    #print(obj.data.materials[0].name)
+    if obj.data.materials[0].name in material_to_check:
+        return True
+    else:
+        return False
+
+def get_material_name(obj):
+    if check_if_cube_has_material(obj):
+        return obj.data.materials[0].name
+    else:
+        return None
+    
+def material_to_rgba(material):
+    principled_bsdf_material = material.node_tree.nodes.get("Principled BSDF")
+    r = principled_bsdf_material.inputs["Base Color"].default_value[0]
+    g = principled_bsdf_material.inputs["Base Color"].default_value[1]
+    b = principled_bsdf_material.inputs["Base Color"].default_value[2]
+    a = principled_bsdf_material.inputs["Base Color"].default_value[3]
+    return r,g,b,a
+    
+    
 #grid_manager = include_module('grid_manager.py')
 #grid = grid_manager.create_grid(2,3,4,cube_size=1)
 #surface =  grid.select_surface_xz(0)
 #material = create_material('test_material',1,0,0,0)
 #for i in surface:
 #    assign_material(i,material)
-=======
-delete_all_materials()
-new_mat = create_material('sp',1,0,0,0)
-assign_material(obj,new_mat)
->>>>>>> f01dad02c8c7a36ad04d9bfe3de869b0e3260524
+
